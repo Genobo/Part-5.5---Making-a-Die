684ece04109f1684979a97a3d9efc4329d84a96d
Gabriel Lindsay <339516205@GoTVDSB.ca>
2023-11-08 11:50:49 -0500

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5._5___Making_a_Die
+{
+    public class Die
+    {
+        private int _roll;
+        private Random _generator;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        public Die(int roll)
+        {
+            _generator = new Random();
+            _roll = roll;
+        }
+
+        public int Roll
+        {
+            get { return _roll; }
+        }
+
+
+
+        public override string ToString()
+        {
+            return _roll + "";
+        }
+
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+
+        public void DrawDie()
+        {
+            if(_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| ? |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Part_5._5___Making_a_Die
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die(3);
+
+            Console.WriteLine($"You rolled a {die1.Roll}");
+            die1.DrawDie();
+            die2.DrawDie(); // will draw a three
+
+            die1.RollDie();
+            die1.DrawDie();
+            //Console.WriteLine($"You rolled a {die1}");
+        }
+    }
+}
